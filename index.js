bb13d05ccaeb8b0082eb219b34b021868f83848f
patriceandrea <andrea_pavico98@yahoo.ca>
2022-10-22 19:31:02 -0400

    setups the package.json

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/shopApp', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("MONGO CONNECTION OPEN! ")
+  })
+  .catch((err) => {
+    console.log("Oh no MONGO CONNECTION error!!")
+    console.log(err)
+  });
+
+
+
+app.set('veiws', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs');
+
+app.get('/dog', (req, res) => {
+  res.send('WOOF!')
+})
+
+app.listen(3000, () => {
+  console.log("APP IS LISTENNING ON 3000");
+})
+
