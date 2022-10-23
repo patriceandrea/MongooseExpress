8d00d27a64b1b4bce40f29cfc834ca0ccf6739b3
patriceandrea <andrea_pavico98@yahoo.ca>
2022-10-22 20:05:04 -0400

    setups model

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const Product = require("./models/product");
 
-mongoose.connect('mongodb://localhost:27017/shopApp', { useNewUrlParser: true, useUnifiedTopology: true })
+
+mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log("MONGO CONNECTION OPEN! ")
+    console.log("MONGO CONNECTION OPEN!")
   })
   .catch((err) => {
     console.log("Oh no MONGO CONNECTION error!!")
@@ -14,7 +16,7 @@ mongoose.connect('mongodb://localhost:27017/shopApp', { useNewUrlParser: true, u
 
 
 
-app.set('veiws', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs');
 
 app.get('/dog', (req, res) => {
