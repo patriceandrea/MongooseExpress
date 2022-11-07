061e5a71ecacbeb3fa6ae99eba506deecf12af19
patriceandrea <andrea_pavico98@yahoo.ca>
2022-11-07 15:31:20 -0500

    adds link to show views

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get('/farms/new', (req, res) => {
 })
 
 app.get('/farms/:id', async (req, res) => {
-  const farm = await Farm.findById(req.params.id);
+  const farm = await Farm.findById(req.params.id).populate('products');
   res.render('farms/show', { farm })
 })
 
@@ -45,9 +45,10 @@ app.post('/farms', async (req, res) => {
   res.redirect('/farms')
 })
 
-app.get('/farms/:id/products/new', (req, res) => {
+app.get('/farms/:id/products/new', async (req, res) => {
   const { id } = req.params;
-  res.render('products/new', { categories, id });
+  const farm = await Farm.findById(id)
+  res.render('products/new', { categories, farm });
 });
 
 app.post('/farms/:id/products', async (req, res) => {
@@ -59,7 +60,7 @@ app.post('/farms/:id/products', async (req, res) => {
   product.farm = farm;
   await farm.save()
   await product.save()
-  res.send(farm);
+  res.redirect(`/farms/${farm._id}`);
 });
 
 //Product Routes
@@ -89,7 +90,8 @@ app.get('/products/new', (req, res) => {
 
 app.get('/products/:id', async (req, res) => {
   const { id } = req.params;
-  const product = await Product.findById(id);
+  const product = await Product.findById(id).populate('farm', 'name');
+  console.log(product)
   res.render('products/show', { product })
 })
 
